60606deffbf0cd285aa8f2efe59a10f83d63f523
Brayan Santos <santos.a.brayan97@gmail.com>
2021-11-23 23:57:28 -0500

    code update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+
+connectDB();
 
 
 app.get('/', (req,res) => {
